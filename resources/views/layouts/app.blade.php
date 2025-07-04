03168cbb34c47a19de354348e5f0d838cfde057a
youdean <34049141+youdean@users.noreply.github.com>
2025-07-04 23:16:02 +0700

    Add sidebar offcanvas menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,11 @@
     <!-- Navbar di sini -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark mb-4">
         <div class="container">
+            @auth
+            <button class="navbar-toggler me-2" type="button" data-bs-toggle="offcanvas" data-bs-target="#sidebar" aria-controls="sidebar">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            @endauth
             <a class="navbar-brand" href="{{ url('/') }}">Sisfo Akademik</a>
             <div class="d-flex ms-auto">
                 @auth
@@ -29,6 +34,24 @@
             </div>
         </div>
     </nav>
+    @auth
+    <div class="offcanvas offcanvas-start" tabindex="-1" id="sidebar" aria-labelledby="sidebarLabel">
+        <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="sidebarLabel">Menu</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body p-0">
+            <div class="list-group list-group-flush">
+                <a href="{{ route('dashboard') }}" class="list-group-item list-group-item-action">Dashboard</a>
+                <a href="{{ route('guru.index') }}" class="list-group-item list-group-item-action">Manajemen Guru</a>
+                <a href="{{ route('siswa.index') }}" class="list-group-item list-group-item-action">Manajemen Siswa</a>
+                <a href="{{ route('mapel.index') }}" class="list-group-item list-group-item-action">Manajemen Mapel</a>
+                <a href="{{ route('nilai.index') }}" class="list-group-item list-group-item-action">Nilai Siswa</a>
+                <a href="{{ route('absensi.index') }}" class="list-group-item list-group-item-action">Absensi Siswa</a>
+            </div>
+        </div>
+    </div>
+    @endauth
     <div class="container">
         @yield('content')
     </div>
