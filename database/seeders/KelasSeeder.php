276acbaa40aa2f9cf665c45b77c0a54d625a1b12
youdean <34049141+youdean@users.noreply.github.com>
2025-07-06 17:03:53 +0700

    feat: add majors for upper grades

--- database/seeders/KelasSeeder.php
+++ database/seeders/KelasSeeder.php
@@ -10,11 +10,21 @@ class KelasSeeder extends Seeder
     public function run(): void
     {
         $data = [];
-        foreach ([10,11,12] as $grade) {
-            foreach (['A','B','C'] as $suffix) {
-                $data[] = ['nama' => $grade . $suffix];
+
+        // Grade 10 does not have majors
+        foreach (['A', 'B', 'C'] as $suffix) {
+            $data[] = ['nama' => '10' . $suffix];
+        }
+
+        // Grades 11 and 12 have IPA and IPS majors
+        foreach ([11, 12] as $grade) {
+            foreach (['IPA', 'IPS'] as $major) {
+                foreach (['A', 'B'] as $suffix) {
+                    $data[] = ['nama' => $grade . ' ' . $major . ' ' . $suffix];
+                }
             }
         }
+
         DB::table('kelas')->insert($data);
     }
 }
--- database/seeders/PengajaranSeeder.php
+++ database/seeders/PengajaranSeeder.php
@@ -10,7 +10,11 @@ class PengajaranSeeder extends Seeder
     public function run(): void
     {
         $data = [];
-        $kelasList = ['10A', '10B', '10C'];
+        $kelasList = [
+            '10A', '10B', '10C',
+            '11 IPA A', '11 IPA B', '11 IPS A', '11 IPS B',
+            '12 IPA A', '12 IPA B', '12 IPS A', '12 IPS B',
+        ];
         $index = 0;
         for ($mapel = 1; $mapel <= 10; $mapel++) {
             foreach ($kelasList as $kelas) {
