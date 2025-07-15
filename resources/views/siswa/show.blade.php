485a4990e6be75396b306cdcb7ee96e3d1a570b5
youdean <34049141+youdean@users.noreply.github.com>
2025-07-15 16:53:14 +0700

    Tampilkan email di halaman data diri siswa

--- resources/views/siswa/show.blade.php
+++ resources/views/siswa/show.blade.php
@@ -7,6 +7,7 @@
 <table class="table table-bordered w-50">
     <tr><th>Nama</th><td>{{ $siswa->nama }}</td></tr>
     <tr><th>NISN</th><td>{{ $siswa->nisn }}</td></tr>
+    <tr><th>Email</th><td>{{ $siswa->user?->email ?? '-' }}</td></tr>
     <tr><th>Kelas</th><td>{{ $siswa->kelas }}</td></tr>
     <tr><th>Tempat Lahir</th><td>{{ $siswa->tempat_lahir }}</td></tr>
     <tr><th>Jenis Kelamin</th><td>{{ $siswa->jenis_kelamin }}</td></tr>
