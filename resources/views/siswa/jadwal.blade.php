02fb8acbb698d9055153301aa32accb418703dee
youdean <34049141+youdean@users.noreply.github.com>
2025-07-30 10:02:25 +0700

    Disable future schedule attendance and add test

--- resources/views/siswa/jadwal.blade.php
+++ resources/views/siswa/jadwal.blade.php
@@ -4,7 +4,13 @@
 
 @section('content')
 <h1 class="mb-3">Jadwal Pelajaran Saya (Kelas {{ $siswa->kelas }})</h1>
-@php $days = ['Senin','Selasa','Rabu','Kamis','Jumat']; @endphp
+@php
+    use Carbon\Carbon;
+    $days = ['Senin','Selasa','Rabu','Kamis','Jumat'];
+    $dayMap = ['Minggu' => 0, 'Senin' => 1, 'Selasa' => 2, 'Rabu' => 3, 'Kamis' => 4, 'Jumat' => 5, 'Sabtu' => 6];
+    $currentDayIndex = Carbon::now()->dayOfWeek;
+    $currentTime = Carbon::now()->format('H:i');
+@endphp
 @foreach($days as $day)
     <h4 class="mt-4">{{ $day }}</h4>
     <table class="table table-bordered">
@@ -18,12 +24,20 @@
         </thead>
         <tbody>
             @forelse($jadwal->get($day, collect()) as $j)
+            @php
+                $jadwalIndex = $dayMap[$j->hari] ?? 7;
+                $isFuture = $jadwalIndex > $currentDayIndex || ($jadwalIndex == $currentDayIndex && $j->jam_mulai > $currentTime);
+            @endphp
             <tr>
                 <td>{{ $j->mapel->nama }}</td>
                 <td>{{ $j->guru->nama }}</td>
                 <td>{{ $j->jam_mulai }} - {{ $j->jam_selesai }}</td>
                 <td>
-                    <a href="{{ route('student.jadwal.absen.form', $j->id) }}" class="btn btn-sm btn-primary">Ambil Absen</a>
+                    @if($isFuture)
+                        <button class="btn btn-sm btn-primary" disabled>Ambil Absen</button>
+                    @else
+                        <a href="{{ route('student.jadwal.absen.form', $j->id) }}" class="btn btn-sm btn-primary">Ambil Absen</a>
+                    @endif
                 </td>
             </tr>
             @empty
