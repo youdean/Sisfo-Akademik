c8ce89de96669e9d7834649ec923b734db771105
youdean <maulana24hasanuddin@gmail.com>
2025-08-07 11:05:14 +0700

    Update logo

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -17,7 +17,7 @@
                 <div class="col-md-6">
                     <div class="text-center mb-4">
                         <a href="/">
-                            <img src="https://smasmuhammadiyah.sch.id/wp-content/uploads/2024/10/logo-smasmuhammadiyah.png" alt="Logo" class="img-fluid" style="max-height: 80px;" />
+                            <img src="{{ asset('images/logo-smasmuhammadiyah.png') }}" alt="Logo" class="img-fluid" style="max-height: 80px;" />
                         </a>
                     </div>
                     <div class="card shadow-sm">
