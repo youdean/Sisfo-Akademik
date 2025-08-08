2ca7013725bdc3e7aa2f997480440cf623fffbac
youdean <34049141+youdean@users.noreply.github.com>
2025-08-09 02:15:00 +0700

    Add feature test for disabled future lesson button

--- resources/views/absensi/pelajaran.blade.php
+++ resources/views/absensi/pelajaran.blade.php
@@ -57,13 +57,22 @@
     @endforelse
 </ul>
 @else
+    @php $now = \Carbon\Carbon::now(); @endphp
     @foreach($days as $day)
         <h4 class="mt-4">{{ $day }}</h4>
         <ul class="list-group mb-3">
             @forelse($jadwal->get($day, collect()) as $j)
+                @php
+                    $start = \Carbon\Carbon::parse($dates[$day] . ' ' . $j->jam_mulai);
+                    $end = \Carbon\Carbon::parse($dates[$day] . ' ' . $j->jam_selesai);
+                @endphp
                 <li class="list-group-item d-flex justify-content-between align-items-center">
                     <span>{{ $j->kelas->nama }} - {{ $j->mapel->nama }} ({{ $j->jam_mulai }} - {{ $j->jam_selesai }})</span>
-                    <a href="{{ route('absensi.pelajaran.form', ['jadwal' => $j->id, 'tanggal' => $dates[$day]]) }}" class="btn btn-sm btn-primary">Isi Absen</a>
+                    @if($now->between($start, $end))
+                        <a href="{{ route('absensi.pelajaran.form', ['jadwal' => $j->id, 'tanggal' => $dates[$day]]) }}" class="btn btn-sm btn-primary">Isi Absen</a>
+                    @else
+                        <button class="btn btn-sm btn-primary" disabled>Isi Absen</button>
+                    @endif
                 </li>
             @empty
                 <li class="list-group-item text-center">Tidak ada jadwal</li>
